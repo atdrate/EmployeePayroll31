dcaf4228549f40662d2008f7df20b746a00f042b
subodhNagalwade <subodhnagalwade@gmail.com>
2022-10-14 12:41:13 +0530

    Insert Data in table

--- PayrollQuery.sql
+++ PayrollQuery.sql
@@ -11,3 +11,12 @@ Salary varchar(10),
 StartDate varchar(20)
 )
  select * from Employee_Payroll
+
+ --------UC3 insert data in the table------
+insert into employee_payroll values 
+('Riya',25000,'02-08-2019'),
+('Shraddha',20000.20,'22-09-2015'),
+('Puja',12000.33,'19-11-2013'),
+('Mrunal',22500.200,'13-05-2019'),
+('Shreya',36123,'29-02-2020')
+
