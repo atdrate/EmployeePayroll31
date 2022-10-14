27fbca2cafe7a06bcc8698f90f3e94ddce5ebacf
subodhNagalwade <subodhnagalwade@gmail.com>
2022-10-14 12:36:45 +0530

    Create Employee_Payroll atable

--- PayrollQuery.sql
+++ PayrollQuery.sql
@@ -1,3 +1,13 @@
 ---uc1--
 create database payroll_services
 use payroll_services
+
+------UC2 Create a employee payroll table------
+create table Employee_Payroll
+(
+Id int identity primary key,
+Name varchar(20),
+Salary varchar(10),
+StartDate varchar(20)
+)
+ select * from Employee_Payroll
